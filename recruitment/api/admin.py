6ab353b5fa2ffc401d7cae87867bd46ce257355b
Shubh Gupta <shubhngupta04@gmail.com>
2020-11-26 18:58:00 +0530

    swagger documentation and login register view created

--- recruitment/api/admin.py
+++ recruitment/api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import (Domain,mcqQuestions,typeQuestions,Responses)
+from .models import (Domain,mcqQuestions,typeQuestions,Responses,User)
 
+admin.site.register(User)
 
 # Register your models here.
 @admin.register(Domain)
--- recruitment/api/serializers.py
+++ recruitment/api/serializers.py
@@ -1,7 +1,66 @@
 from rest_framework import serializers
-from .models import Domain,mcqQuestions,typeQuestions,Responses
-from django.contrib.auth.models import User
+from .models import Domain,mcqQuestions,typeQuestions,Responses,User
 # from rest_framework.authtoken.models import Token
+from django.contrib import auth
+from rest_framework.exceptions import AuthenticationFailed
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        max_length=68, min_length=6, write_only=True)
+
+    default_error_messages = {
+        'username': 'The username should only contain alphanumeric characters'}
+
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'password']
+
+    def validate(self, attrs):
+        email = attrs.get('email', '')
+        username = attrs.get('username', '')
+
+        if not username.isalnum():
+            raise serializers.ValidationError(
+                self.default_error_messages)
+        return attrs
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
+# class EmailVerificationSerializer(serializers.ModelSerializer):
+#     token = serializers.CharField(max_length=555)
+#
+#     class Meta:
+#         model = User
+#         fields = ['token']
+
+class LoginSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(max_length=255, min_length=3)
+    password = serializers.CharField(max_length=68, min_length=6,write_only=True)
+    username = serializers.CharField(max_length=255, min_length=3,read_only=True)
+    tokens = serializers.CharField(max_length=68, min_length=6, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['email','password','username','tokens']
+
+    def validate(self, attrs):
+        email = attrs.get('email','')
+        password = attrs.get('password','')
+        user = auth.authenticate(email=email,password=password)
+
+        if not user:
+            raise AuthenticationFailed('Invalid credentials, try again')
+        if not user.is_active:
+            raise AuthenticationFailed('Account disabled, contact admin')
+
+
+        return {
+            'email': user.email,
+            'username': user.username,
+            'tokens': user.tokens
+        }
 
 
 class mcqSerializer(serializers.ModelSerializer):
