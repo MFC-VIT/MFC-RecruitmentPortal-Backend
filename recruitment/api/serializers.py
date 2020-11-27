bcab2b5c4efcdd8d2b77d9b999a1951bbdf98508
Shubh Gupta <shubhngupta04@gmail.com>
2020-11-27 19:25:00 +0530

    logout view created

--- recruitment/api/serializers.py
+++ recruitment/api/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Domain,mcqQuestions,typeQuestions,Responses,User
 from django.contrib import auth
 from rest_framework.exceptions import AuthenticationFailed
+from rest_framework_simplejwt.tokens import RefreshToken, TokenError
 
 
 class RegisterSerializer(serializers.ModelSerializer):
@@ -70,3 +71,22 @@ class responseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Responses
         fields = ('domain','question','answer')
+
+class LogoutSerializer(serializers.Serializer):
+    refresh = serializers.CharField()
+
+    default_error_messages = {
+        'bad_token': ('Token is expired or invalid')
+    }
+
+    def validate(self, attrs):
+        self.token = attrs['refresh']
+        return attrs
+
+    def save(self, **kwargs):
+
+        try:
+            RefreshToken(self.token).blacklist()
+
+        except TokenError:
+            self.fail('bad_token')
