dc4a2723d9ab0b828c4df9cdfcae6701fb71756e
Akshat Gupta <akshatgupta1903@gmail.com>
2022-02-02 13:48:52 +0530

    Update settings.py
    default email added

--- recruitment/settings.py
+++ recruitment/settings.py
@@ -169,4 +169,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 django_heroku.settings(locals())
