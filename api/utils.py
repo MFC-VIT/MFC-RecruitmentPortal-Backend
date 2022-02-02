d4646b48b1c5ccbd9206f095e008872066683ded
Akshat Gupta <akshatgupta1903@gmail.com>
2022-02-02 13:49:48 +0530

    Update utils.py
    email sender changed

--- api/utils.py
+++ api/utils.py
@@ -7,5 +7,12 @@ import yagmail
 class Util:
     @staticmethod
     def send_email(data):
-        yag = yagmail.SMTP(user=os.environ.get('EMAIL_HOST_USER'), password=os.environ.get('EMAIL_HOST_PASSWORD'))
-        yag.send(to=data['to_email'], subject=data['email_subject'], contents=data['email_body'])
+#         yag = yagmail.SMTP(user=os.environ.get('EMAIL_HOST_USER'), password=os.environ.get('EMAIL_HOST_PASSWORD'))
+#         yag.send(to=data['to_email'], subject=data['email_subject'], contents=data['email_body'])
+        send_mail(
+            data["email_subject"],
+            data["email_body"],
+            None,
+            [data['to_email']],
+            fail_silently=False,
+        )
